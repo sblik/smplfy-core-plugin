5eeb021109c087e24741f29ab36c8719b8d863f4
Jayd Page <jayd@maxcutsoftware.com>
2024-02-27 11:43:17 +0200

    Add plugin entry file

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # bs-core-plugin
+
 Core logic for a unified development approach across multiple plugins
+
+## Prerequisites
+
+- Gravity Forms 2.8.4 +
+- PHP 7.3 +
---
+++ bs-core/bs-core.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * BS Core
+ *
+ * @package BS Core
+ * @author Simplify Small Biz
+ * @since 1.0.0
+ *
+ * @wordpress-plugin
+ * Plugin Name: BS Core
+ * Version: 1.0.0
+ * Description: Core logic for a unified development approach across multiple plugins.
+ * Author: Simplify Small Biz
+ * Author URI: https://simplifybiz.com
+ * Requires PHP: 7.3
+ */
