1ddf2616f84d1620f63f28932df70dcde531b22b
TomCom1234 <tomcom12344@gmail.com>
2024-09-17 14:59:02 +0100

    Update tests with namespacing

--- tests/repositories/TestConcreteRepository.php
+++ tests/repositories/TestConcreteRepository.php
@@ -1,5 +1,5 @@
 <?php
-
+use SmplfyCore\SMPLFY_BaseRepository;
 require_once __DIR__ . '/../../smplfy-core/includes/repositories/SMPLFY_BaseRepository.php';
 
 class TestConcreteRepository extends SMPLFY_BaseRepository {
