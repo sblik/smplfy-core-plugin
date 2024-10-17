3687abccc3524906113619e98983a174607b0b07
andrenellin <andrenellin@gmail.com>
2024-10-17 07:50:50 -0600

    Update name

--- smplfy-core/smplfy-core.php
+++ smplfy-core/smplfy-core.php
@@ -4,13 +4,13 @@
  *
  * @package SMP Core
  * @author  Simplify Small Biz
- * @since   1.0.0
+ * @since   1.0.1
  *
  * @wordpress-plugin
  * Plugin Name: Smplfy Core
- * Version: 1.0.0
+ * Version: 1.0.1
  * Description: Core logic for a unified development approach across multiple plugins.
- * Author: Simplify Small Biz
+ * Author: Simplify Biz
  * Author URI: https://simplifybiz.com
  * Requires PHP: 7.4
  */
