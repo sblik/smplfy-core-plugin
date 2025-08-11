e76c3550b66bf1da367251a446c7635b12884e41
TomCom1234 <tomcom12344@gmail.com>
2025-08-11 16:40:45 +0100

    Add method to get a user by meta value

--- smplfy-core/includes/wp-api/UserMeta.php
+++ smplfy-core/includes/wp-api/UserMeta.php
@@ -3,6 +3,9 @@
 namespace SmplfyCore;
 
 
+use WP_User;
+use WP_User_Query;
+
 class UserMeta {
 	public static $phoneMetaRow = 'user_phone';
 
@@ -27,6 +30,34 @@ class UserMeta {
 	 */
 	public static function retrieve_user_meta( $userID, $metaKey ) {
 		return get_user_meta( $userID, $metaKey, true );
+	}
+
+
+	/**
+	 * @param $metaKey
+	 * @param $metaValue
+	 *
+	 * @return WP_User|null
+	 */
+	public static function get_user_by_meta( $metaKey, $metaValue ): ?WP_User {
+
+		$args = [
+			'meta_key'   => $metaKey,
+			'meta_value' => $metaValue,
+			'number'     => 1, // Limit to one result
+		];
+
+		$user_query = new WP_User_Query( $args );
+		if ( ! empty( $user_query->get_results() ) ) {
+			$users = $user_query->get_results();
+			foreach ( $users as $user ) {
+				return $user;
+			}
+		} else {
+			return null;
+		}
+
+		return null;
 	}
 
 	/**
